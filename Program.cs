6d36e8e41f0c75f414168a98d05cd494ca1b8602
ClintonSBarnes <clintbarnes8@gmail.com>
2022-05-24 19:05:19 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] array = new int[] { 5, 51, -7, 13, -99, 15, -8, 45, 90 };
+float total=0;
+
+foreach (int number in array)
+{
+    total += number;
+}
+
+float average = (float)total / array.Length;
+Console.WriteLine(average);
